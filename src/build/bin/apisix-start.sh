9d4ade3fe18cda11c8a421e490b29b5564e97dae
wklken <wklken@gmail.com>
2023-06-01 18:22:34 +0800

    feat(bin/apisix-start.sh): generate empty apisix.yaml for standalone mode

--- src/build/bin/apisix-start.sh
+++ src/build/bin/apisix-start.sh
@@ -1,25 +1,32 @@
 #!/bin/sh
 echo "starting......"
 
+
+# if standaloneConfigPath not empty, watch the path
+if [ x"${standaloneConfigPath}" != x"" ]
+then
+    echo "it's standalone mode, will watch config file first, the start apisix"
+    if [ ! -f /usr/local/apisix/conf/apisix.yaml ]
+    then
+        echo "generate an empty apisix.yaml, while the operator not sync the data into apisix.yaml"
+        printf "routes: []\n#END" > /usr/local/apisix/conf/apisix.yaml
+    fi
+
+    echo "start config-watcher for ${standaloneConfigPath}"
+    sh /data/bkgateway/bin/config-watcher-start.sh "-sourcePath ${standaloneConfigPath} -destPath /usr/local/apisix/conf -files apisix.yaml" &
+    echo "config-watcher for ${standaloneConfigPath} started"
+fi
+
 # start
 echo "start apisix"
 apisix start
 echo "apisix started"
-# sleep 5秒以规避apisix启动时空的apisix.yaml会使init_worker阶段失败的问题
-sleep 5
 
 # start nginx error to sentry
 if [ x"${BK_APIGW_NGINX_ERROR_LOG_SENTRY_DSN}" != x"" ]
 then
     echo "start sentrylogs to ship nginx error log to sentry"
     SENTRY_DSN="${BK_APIGW_NGINX_ERROR_LOG_SENTRY_DSN}" NGINX_ERROR_PATH="/usr/local/apisix/logs/error.log" sentrylogs --daemonize
-fi
-# if standaloneConfigPath not empty, watch the path
-if [ x"${standaloneConfigPath}" != x"" ]
-then
-    echo "start config-watcher for ${standaloneConfigPath}"
-    sh /data/bkgateway/bin/config-watcher-start.sh "-sourcePath ${standaloneConfigPath} -destPath /usr/local/apisix/conf -files apisix.yaml" &
-    echo "config-watcher for ${standaloneConfigPath} started"
 fi
 
 echo "start config-watcher for ${apisixDebugConfigPath}(note: will wait until the container quit)"
