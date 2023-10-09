284bb1609422080831e99cf3e26c6bb4f7a52b1d
alex-smile <443677891@qq.com>
2023-10-09 17:51:05 +0800

    fix test-access-token tests

--- src/apisix/editions/ee/tests/bk-cache/test-access-token.lua
+++ src/apisix/editions/ee/tests/bk-cache/test-access-token.lua
@@ -16,32 +16,21 @@
 -- to the current version of the project delivered to anyone in the future.
 --
 local access_token_cache = require("apisix.plugins.bk-cache.access-token")
-local bkauth_component = require("apisix.plugins.bk-components.bkauth")
 local ssm_component = require("apisix.plugins.bk-components.ssm")
 local uuid = require("resty.jit-uuid")
 
 describe(
     "access_token cache", function()
 
-        local bkauth_verify_access_token_result
-        local bkauth_verify_access_token_err
         local ssm_verify_access_token_result
         local ssm_verify_access_token_err
         local ssm_is_configured
 
         before_each(
             function()
-                bkauth_verify_access_token_result = nil
-                bkauth_verify_access_token_err = nil
                 ssm_verify_access_token_result = nil
                 ssm_verify_access_token_err = nil
                 ssm_is_configured = false
-
-                stub(
-                    bkauth_component, "verify_access_token", function()
-                        return bkauth_verify_access_token_result, bkauth_verify_access_token_err
-                    end
-                )
 
                 stub(
                     ssm_component, "verify_access_token", function()
@@ -59,7 +48,6 @@ describe(
 
         after_each(
             function()
-                bkauth_component.verify_access_token:revert()
                 ssm_component.verify_access_token:revert()
                 ssm_component.is_configured:revert()
             end
@@ -67,31 +55,8 @@ describe(
 
         context(
             "local get_access_token", function()
-                -- it(
-                --     "bkauth verify ok", function()
-                --         bkauth_verify_access_token_result = {
-                --             bk_app_code = "my-app",
-                --             username = "admin",
-                --             expires_in = 10,
-                --         }
-                --         bkauth_verify_access_token_err = nil
-
-                --         local result = access_token_cache._get_access_token("fake-access-token")
-                --         assert.is_same(
-                --             result.token, {
-                --                 app_code = "my-app",
-                --                 user_id = "admin",
-                --                 expires_in = 10,
-                --             }
-                --         )
-                --         assert.is_nil(result.err)
-                --     end
-                -- )
-
                 it(
-                    "bkauth verify fail, ssm verify ok", function()
-                        bkauth_verify_access_token_result = nil
-                        bkauth_verify_access_token_err = "bkauth err"
+                    "ssm verify ok", function()
                         ssm_verify_access_token_result = {
                             bk_app_code = "my-foo",
                             username = "kitty",
@@ -113,9 +78,7 @@ describe(
                 )
 
                 it(
-                    "bkauth verify fail, ssm verify fail", function()
-                        bkauth_verify_access_token_result = nil
-                        bkauth_verify_access_token_err = "bkauth error"
+                    "ssm verify fail, and is configured", function()
                         ssm_verify_access_token_result = nil
                         ssm_verify_access_token_err = "ssm error"
                         ssm_is_configured = true
@@ -127,23 +90,19 @@ describe(
                 )
 
                 it(
-                    "bkauth verify fail, ssm is not configured", function()
-                        bkauth_verify_access_token_result = nil
-                        bkauth_verify_access_token_err = "bkauth error"
+                    "ssm verify fail, but not configured", function()
                         ssm_verify_access_token_result = nil
                         ssm_verify_access_token_err = "ssm error"
                         ssm_is_configured = nil
 
                         local result = access_token_cache._get_access_token("fake-access-token")
                         assert.is_nil(result.token)
-                        -- assert.is_equal(result.err, "bkauth error")
+                        assert.is_equal(result.err, "authentication based on access_token is not supported")
                     end
                 )
 
                 it(
-                    "bkauth verify fail, ssm is not configured", function()
-                        bkauth_verify_access_token_result = nil
-                        bkauth_verify_access_token_err = "bkauth error"
+                    "ssm verify ok, but not configured", function()
                         ssm_verify_access_token_result = {
                             bk_app_code = "my-foo",
                             username = "kitty",
@@ -154,7 +113,7 @@ describe(
 
                         local result = access_token_cache._get_access_token("fake-access-token")
                         assert.is_nil(result.token)
-                        -- assert.is_equal(result.err, "bkauth error")
+                        assert.is_equal(result.err, "authentication based on access_token is not supported")
                     end
                 )
             end
