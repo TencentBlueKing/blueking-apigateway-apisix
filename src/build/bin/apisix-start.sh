e6e824b05eb2190a20df691d68ecb504a6143e63
wklken <wklken@gmail.com>
2026-08-04 17:18:00 +0800

    fix(build): avoid sentrylogs daemonization (#178)
    (cherry picked from commit 48d9a3083d47a17c351313178e4af7d631cd1b05)

--- src/build/bin/apisix-start.sh
+++ src/build/bin/apisix-start.sh
@@ -8,7 +8,7 @@ echo "starting......"
 if [ x"${BK_APIGW_NGINX_ERROR_LOG_SENTRY_DSN}" != x"" ]
 then
     echo "start sentrylogs to ship nginx error log to sentry"
-    SENTRY_DSN="${BK_APIGW_NGINX_ERROR_LOG_SENTRY_DSN}" NGINX_ERROR_PATH="/usr/local/apisix/logs/error.log" sentrylogs --daemonize
+    SENTRY_DSN="${BK_APIGW_NGINX_ERROR_LOG_SENTRY_DSN}" NGINX_ERROR_PATH="/usr/local/apisix/logs/error.log" sentrylogs >/dev/null 2>&1 &
 
     # start a daemon to check sentrylogs, the sentrylogs will only process the `tail` records, so it's safe to restart it
     sh /data/bkgateway/bin/sentrylogs-daemonize.sh &
